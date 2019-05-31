668c0ab3a47ac67b168bf34c9db2286c291452d8
brucewuu <402432291@qq.com>
2019-05-31 15:29:04 +0800

    commit spring-ultron mybatis

---
+++ ultron-mybatis/src/main/java/org/springultron/dao/BaseEntity.java
@@ -0,0 +1,54 @@
+package org.springultron.dao;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 数据库操作实体基类
+ *
+ * @Auther: brucewuu
+ * @Date: 2019-05-28 10:55
+ * @Description:
+ */
+public abstract class BaseEntity implements Serializable {
+    /**
+     * 数据库ID主键
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    /**
+     * 数据创建时间
+     */
+    private LocalDateTime createAt;
+    /**
+     * 数据修改时间
+     */
+    private LocalDateTime updateAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+}
