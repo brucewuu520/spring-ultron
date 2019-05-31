a746531bd2273b6392fe3a31f2c5eb32b8a533ac
brucewuu <402432291@qq.com>
2019-05-31 15:29:19 +0800

    commit spring-ultron redis

---
+++ ultron-redis/src/main/java/org/springultron/redis/RedisClient.java
@@ -0,0 +1,106 @@
+package org.springultron.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Redis操作客户端
+ *
+ * @Auther: brucewuu
+ * @Date: 2019-05-31 14:26
+ * @Description:
+ */
+@Component
+public class RedisClient {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    public RedisClient(RedisTemplate<String, Object> redisTemplate, StringRedisTemplate stringRedisTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public void set(String key, String value) {
+        stringRedisTemplate.opsForValue().set(key, value);
+    }
+
+    public void set(String key, String value, int expireTime) {
+        stringRedisTemplate.opsForValue().set(key, value, expireTime, TimeUnit.SECONDS);
+    }
+
+    public String get(String key) {
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+
+    public void setObject(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void setObject(String key, Object value, int timeout) {
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.SECONDS);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getObject(String key) {
+        return (T) redisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * 设置缓存key过期时间，单位秒
+     *
+     * @param key     缓存key
+     * @param timeout 过期时间
+     * @return 是否设置成功
+     */
+    public boolean expire(String key, long timeout) {
+        Boolean result = redisTemplate.expire(key, timeout, TimeUnit.SECONDS);
+        return null == result ? false : result;
+    }
+
+    /**
+     * 判断缓存中是否有对应的value
+     *
+     * @param key 缓存key
+     * @return 是否存在
+     */
+    public boolean exists(String key) {
+        Boolean isExist = redisTemplate.hasKey(key);
+        return null == isExist ? false : isExist;
+    }
+
+    /**
+     * 批量删除缓存
+     *
+     * @param keys 缓存key数组
+     */
+    public void delete(String... keys) {
+        if (null != keys && keys.length > 0) {
+            if (keys.length == 1) {
+                redisTemplate.delete(keys[0]);
+            } else {
+                redisTemplate.delete(Arrays.asList(keys));
+            }
+        }
+    }
+
+    /**
+     * 模糊匹配批量删除
+     *
+     * @param pattern 匹配的前缀
+     */
+    public void deleteByPattern(String pattern) {
+        Set<String> keys = redisTemplate.keys(pattern);
+        if (!CollectionUtils.isEmpty(keys)) {
+            redisTemplate.delete(keys);
+        }
+    }
+}
---
+++ ultron-redis/src/main/java/org/springultron/redis/RedisConfig.java
@@ -0,0 +1,127 @@
+package org.springultron.redis;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author brucewuu
+ * @date 2017/8/20 9:33
+ * @description redis配置  配置序列化方式以及缓存管理器 @EnableCaching 可开启方法注解缓存
+ */
+@EnableCaching
+@Configuration
+@AutoConfigureBefore(RedisAutoConfiguration.class)
+public class RedisConfig extends CachingConfigurerSupport {
+
+    /**
+     * 自定义的缓存key的生成策略
+     * 若想使用这个key  只需要讲注解上keyGenerator的值设置为keyGenerator即可</br>
+     *
+     * @return 自定义策略生成的key
+     */
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(method.getName());
+            for (Object obj : params) {
+                sb.append(obj.toString());
+            }
+            return sb.toString();
+        };
+    }
+
+    /**
+     * json序列化
+     *
+     * @return 使用Jackson
+     */
+    private Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer() {
+        // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.findAndRegisterModules();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        serializer.setObjectMapper(objectMapper);
+        return serializer;
+    }
+
+    /**
+     * 配置自定义redisTemplate
+     *
+     * @param redisConnectionFactory 连接池配置
+     * @return RedisTemplate
+     */
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Bean
+    @ConditionalOnMissingBean(RedisTemplate.class)
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // value 序列化方式采用 jackson
+        RedisSerializer jackson2JsonRedisSerializer = jackson2JsonRedisSerializer();
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(stringRedisSerializer);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+//    /**
+//     * 配置缓存管理器
+//     *
+//     * @param connectionFactory
+//     * @return
+//     */
+//    @Bean
+//    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+//        // 生成一个默认配置，通过config对象即可对缓存进行自定义配置
+//        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+//        // 设置缓存的默认过期时间，也是使用Duration设置
+//        config = config.entryTtl(Duration.ofMinutes(1))
+//                // 设置 key为string序列化
+//                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+//                // 设置value为json序列化
+//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer()))
+//                // 不缓存空值
+//                .disableCachingNullValues();
+//
+//        // 设置一个初始化的缓存空间set集合
+//        Set<String> cacheNames = new HashSet<>();
+//        cacheNames.add("timeGroup");
+//        cacheNames.add("user");
+//        // 对每个缓存空间应用不同的配置
+//        Map<String, RedisCacheConfiguration> configMap = new HashMap<>();
+//        configMap.put("timeGroup", config);
+//        configMap.put("user", config.entryTtl(Duration.ofSeconds(120)));
+//        // 使用自定义的缓存配置初始化一个cacheManager
+//        return RedisCacheManager.builder(redisConnectionFactory)
+//                // 一定要先调用该方法设置初始化的缓存名，再初始化相关的配置
+//                .initialCacheNames(cacheNames)
+//                .withInitialCacheConfigurations(configMap)
+//                .build();
+//    }
+}
