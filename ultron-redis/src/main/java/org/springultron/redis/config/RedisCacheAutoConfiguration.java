b209b058dd5e20191f20412e87677dcf76619460
brucewuu <402432291@qq.com>
2019-11-11 18:29:04 +0800

    基于Redis的Spring cache自动化配置以及注解增强，@CachePut(value="name#300", key="id")，#配置缓存过期时长单位秒

--- ultron-redis/src/main/java/org/springultron/redis/config/RedisCacheAutoConfiguration.java
+++ ultron-redis/src/main/java/org/springultron/redis/config/RedisCacheAutoConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizers;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
@@ -36,6 +37,7 @@ import java.util.stream.Collectors;
  */
 @Configuration(proxyBeanMethods = false)
 @AutoConfigureAfter({RedisAutoConfiguration.class})
+@EnableConfigurationProperties({CacheProperties.class})
 public class RedisCacheAutoConfiguration extends CachingConfigurerSupport {
 
     /**
