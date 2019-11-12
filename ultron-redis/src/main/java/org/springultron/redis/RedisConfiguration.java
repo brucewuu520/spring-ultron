d48ad8caf22c7d23363241c5b12b966ccc3236b2
brucewuu <402432291@qq.com>
2019-11-12 16:16:41 +0800

    优化Redis序列化策略默认使用Jackson序列化，如果没有依赖Jackson则使用JDK序列化

--- ultron-redis/src/main/java/org/springultron/redis/RedisConfiguration.java
+++ ultron-redis/src/main/java/org/springultron/redis/RedisConfiguration.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -29,15 +30,14 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * Redis配置
- * 配置Jackson序列化
- * 配置Redis cache缓存管理器
+ * Redis及Redis cache缓存管理器配置
+ * 序列化策略：
+ * 默认使用Jackson序列化Redis value，当没有依赖Jackson时默认使用jdk序列化
  *
  * @author brucewuu
  * @date 2019-05-31 14:26
  */
 @Configuration
-@ConditionalOnClass({ObjectMapper.class})
 @AutoConfigureBefore({RedisAutoConfiguration.class})
 public class RedisConfiguration extends CachingConfigurerSupport {
 
@@ -63,16 +63,22 @@ public class RedisConfiguration extends CachingConfigurerSupport {
         };
     }
 
+    /**
+     * 自定义Jackson序列化
+     * 可自定义注入redisSerializer Bean替换
+     */
     @Bean
+    @ConditionalOnClass({ObjectMapper.class})
+    @ConditionalOnMissingBean(name = {"redisSerializer"})
     public RedisSerializer<Object> redisSerializer() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         // 反序列化时去掉多余的字段
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        // 日期格式化
-        objectMapper.registerModule(new UltronJavaTimeModule());
+        // java8日期序列化
         objectMapper.findAndRegisterModules();
+        objectMapper.registerModule(new UltronJavaTimeModule());
         return new GenericJackson2JsonRedisSerializer(objectMapper);
     }
 
@@ -97,7 +103,7 @@ public class RedisConfiguration extends CachingConfigurerSupport {
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Bean
     @ConditionalOnMissingBean(name = {"redisTemplate"})
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory, RedisSerializer<Object> redisSerializer) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory, @Autowired(required = false) RedisSerializer<Object> redisSerializer) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         // 配置连接工厂
         template.setConnectionFactory(redisConnectionFactory);
@@ -120,11 +126,11 @@ public class RedisConfiguration extends CachingConfigurerSupport {
     @Primary
     @Bean
     @ConditionalOnMissingBean(name = {"cacheManager"})
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, RedisSerializer<Object> redisSerializer) {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .disableCachingNullValues()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer));
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, @Autowired(required = false) RedisSerializer<Object> redisSerializer) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig().disableCachingNullValues();
+        if (redisSerializer != null) {
+            redisCacheConfiguration = redisCacheConfiguration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer));
+        }
         CacheEnum[] cacheEnums = CacheEnum.values();
         Map<String, RedisCacheConfiguration> cacheConfigMap = new LinkedHashMap<>(cacheEnums.length);
         for (CacheEnum cacheEnum : cacheEnums) {
