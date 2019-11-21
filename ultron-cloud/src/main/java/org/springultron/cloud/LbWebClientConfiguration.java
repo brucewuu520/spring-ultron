59c22ee3dd12ba3c37a9c993f4a44ad0641dfb6f
brucewuu <402432291@qq.com>
2019-11-21 19:38:55 +0800

    1、升级Spring cloud版本Greenwich.SR4 2、升级Alibaba cloud版本2.1.1 3、响应式负责均衡WebClient配置

--- ultron-cloud/src/main/java/org/springultron/cloud/LbWebClientConfiguration.java
+++ ultron-cloud/src/main/java/org/springultron/cloud/LbWebClientConfiguration.java
@@ -1,7 +1,6 @@
 package org.springultron.cloud;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +19,6 @@ import org.springframework.web.reactive.function.client.WebClient;
  * @date 2019-08-23 14:36
  */
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnClass({WebClient.class})
 @ConditionalOnBean({ReactorLoadBalancerExchangeFilterFunction.class})
 public class LbWebClientConfiguration {
 
