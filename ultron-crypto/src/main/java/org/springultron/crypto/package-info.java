f1cfa5d7a71a8bdad9c40122a00002e0bfe06465
brucewuu <402432291@qq.com>
2020-03-20 22:34:31 +0800

    实现常用的加解密算法，AES、DES、RSA、国密SM2、SM4等

--- ultron-crypto/src/main/java/org/springultron/crypto/package-info.java
+++ ultron-crypto/src/main/java/org/springultron/crypto/package-info.java
@@ -2,10 +2,11 @@
  * 加密解密模块，实现了:
  *
  * <pre>
- *  1.摘要加密（digest），例如：MD5、SHA-X、HMAC等
+ *  AES、DES、RSA国密SM2、SM4等
+ *  以及各种秘钥生成工具
  * </pre>
  *
  * @author brucewuu
  * @date 2019/10/27 22:19
  */
-package org.springultron.core.crypto;
+package org.springultron.crypto;
