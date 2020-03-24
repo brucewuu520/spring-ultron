14a3da9ec000bb97ce2f408dfd6c56c119d01b09
brucewuu <402432291@qq.com>
2020-03-24 20:56:23 +0800

    新增获取Annotation方法

--- ultron-core/src/main/java/org/springultron/core/utils/ClassUtils.java
+++ ultron-core/src/main/java/org/springultron/core/utils/ClassUtils.java
@@ -1,10 +1,15 @@
 package org.springultron.core.utils;
 
+import org.springframework.core.BridgeMethodResolver;
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.ParameterNameDiscoverer;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.core.annotation.SynthesizingMethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.web.method.HandlerMethod;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
@@ -29,11 +34,6 @@ public class ClassUtils extends org.springframework.util.ClassUtils {
      * @return MethodParameter
      */
     public static MethodParameter getMethodParameter(Constructor<?> constructor, int parameterIndex) {
-        try {
-            Class<?> clazz = ClassUtils.forName("org.reactivestreams.Publisher", null);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
         MethodParameter methodParameter = new SynthesizingMethodParameter(constructor, parameterIndex);
         methodParameter.initParameterNameDiscovery(PARAMETER_NAME_DISCOVERER);
         return methodParameter;
@@ -50,5 +50,47 @@ public class ClassUtils extends org.springframework.util.ClassUtils {
         MethodParameter methodParameter = new SynthesizingMethodParameter(method, parameterIndex);
         methodParameter.initParameterNameDiscovery(PARAMETER_NAME_DISCOVERER);
         return methodParameter;
+    }
+
+    /**
+     * 获取Annotation
+     *
+     * @param method         Method
+     * @param annotationType 注解类
+     * @param <A>            泛型标记
+     * @return Annotation
+     */
+    @Nullable
+    public static <A extends Annotation> A getAnnotation(Method method, Class<A> annotationType) {
+        Class<?> targetClass = method.getDeclaringClass();
+        Method specificMethod = ClassUtils.getMostSpecificMethod(method, targetClass);
+        specificMethod = BridgeMethodResolver.findBridgedMethod(specificMethod);
+        // 先找方法，再找方法上的类
+        A annotation = AnnotatedElementUtils.findMergedAnnotation(specificMethod, annotationType);
+        if (annotation != null) {
+            return annotation;
+        }
+        // 获取类上面的Annotation，可能包含组合注解，故采用spring的工具类
+        return AnnotatedElementUtils.findMergedAnnotation(specificMethod.getDeclaringClass(), annotationType);
+    }
+
+    /**
+     * 获取Annotation
+     *
+     * @param handlerMethod  {@link HandlerMethod}
+     * @param annotationType 注解类
+     * @param <A>            泛型标记
+     * @return Annotation
+     */
+    @Nullable
+    public static <A extends Annotation> A getAnnotation(HandlerMethod handlerMethod, Class<A> annotationType) {
+        // 先找方法，再找方法上的类
+        A annotation = handlerMethod.getMethodAnnotation(annotationType);
+        if (annotation != null) {
+            return annotation;
+        }
+        // 获取类上面的Annotation，可能包含组合注解，故采用spring的工具类
+        Class<?> beanType = handlerMethod.getBeanType();
+        return AnnotatedElementUtils.findMergedAnnotation(beanType, annotationType);
     }
 }
