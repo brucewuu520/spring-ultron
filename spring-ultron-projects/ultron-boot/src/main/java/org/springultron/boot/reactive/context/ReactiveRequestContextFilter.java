344834efdb932d2ab44a323218a14fdb29820b2f
吴昭 <wx_ab28a47d76e944ad96dc5eedc63fa83d@git.code.tencent.com>
2020-07-31 10:18:47 +0800

    reactive请求缓存

--- spring-ultron-projects/ultron-boot/src/main/java/org/springultron/boot/reactive/context/ReactiveRequestContextFilter.java
+++ spring-ultron-projects/ultron-boot/src/main/java/org/springultron/boot/reactive/context/ReactiveRequestContextFilter.java
@@ -1,9 +1,9 @@
 package org.springultron.boot.reactive.context;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -15,7 +15,7 @@ import reactor.core.publisher.Mono;
  * @author brucewuu
  * @date 2020/7/26 21:22
  */
-@Configuration(proxyBeanMethods = false)
+@Component
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
 public class ReactiveRequestContextFilter implements WebFilter, Ordered {
 
