bab807eb3a607b7c6827014c39f2845757bae854
brucewuu <402432291@qq.com>
2020-08-21 20:34:33 +0800

    微信数据AES解密测试用例

--- spring-ultron-projects/ultron-crypto/src/test/java/org/springultron/crypto/AESTests.java
+++ spring-ultron-projects/ultron-crypto/src/test/java/org/springultron/crypto/AESTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.springframework.util.Base64Utils;
 import org.springultron.core.utils.Hex;
 
+import java.nio.charset.StandardCharsets;
+
 /**
  * @author brucewuu
  * @date 2020/3/20 18:50
@@ -31,5 +33,29 @@ public class AESTests {
         encryptData = AES.encryptHex(algorithms, data, key);
         System.err.println("加密后22：" + encryptData);
         System.err.println("解密后22：" + AES.decrypt(algorithms, encryptData, key));
+    }
+
+    @Test
+    public void decryptWechat() {
+        String sessionKey = "vkMrQmlmrJkEGW9Curka7g==";
+        String encryptedData = "9az8vEfkjog1BdGUDblChpK7tmFTSMvnDaItxZqih9xA1+bEywSbr6b357ZwWYypnTXQvhIEmqgrOZs2FFkFE1NGiSHlLumK+4pbDDt9+vBS0WZh0SuMPNlYtmFT3/sKXmoi6UGZ2Q71JAzM7FilKunFhRZPHj5jjoIUubWu291ufbHqh2dkZytxeZxZRV3fLwcsMFMmWVhXt1W41LFuEg==";
+        String iv = "Gv80JaVDpE4RmhbUoRQEDg==";
+
+        byte[] keyBytes = Base64Utils.decodeFromString(sessionKey);
+        System.err.println(keyBytes.length);
+
+        byte[] dataBytes = Base64Utils.decodeFromString(encryptedData);
+        System.err.println(dataBytes.length);
+
+
+        // byte[] decryptData = AES.decrypt(AesAlgorithms.AES_CBC_PKCS7, dataBytes, keyBytes, Base64Utils.decodeFromString(iv));
+        // System.err.println(decryptData.length);
+        //
+        //
+        // String data = new String(decryptData);
+
+        String data = AES.decrypt(AesAlgorithms.AES_CBC_PKCS7, encryptedData, sessionKey, iv);
+
+        System.err.println(data);
     }
 }
