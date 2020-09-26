564c1f1aec73e5ef701e381d5d6a32d72d0761d1
brucewuu <402432291@qq.com>
2020-09-26 10:05:07 +0800

    升级mybatis plus 3.4.0插件

--- spring-ultron-projects/ultron-mybatis/src/main/java/org/springultron/dao/MybatisPlusConfiguration.java
+++ spring-ultron-projects/ultron-mybatis/src/main/java/org/springultron/dao/MybatisPlusConfiguration.java
@@ -1,12 +1,19 @@
 package org.springultron.dao;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.core.handlers.MybatisEnumTypeHandler;
 import com.baomidou.mybatisplus.core.parser.ISqlParser;
 import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import com.baomidou.mybatisplus.extension.parsers.BlockAttackSqlParser;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.SqlExplainInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.InitializingBean;
@@ -39,45 +46,39 @@ public class MybatisPlusConfiguration implements InitializingBean {
     }
 
     /**
-     * 分页插件
+     * 新的分页插件
+     * 一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor=false 避免缓存出现问题
      */
     @Bean
-    @ConditionalOnMissingBean({PaginationInterceptor.class})
-    public PaginationInterceptor paginationInterceptor() {
-        // 开启 count 的 join 优化,只针对 left join !!!
-        return new PaginationInterceptor().setCountSqlParser(new JsqlParserCountOptimize(true));
+    @ConditionalOnMissingBean(MybatisPlusInterceptor.class)
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件，默认MYSQL
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        // 防止全表更新与删除插件
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        return interceptor;
     }
 
-    /**
-     * 防止全表更新/删除
-     */
     @Bean
-    @ConditionalOnMissingBean({SqlExplainInterceptor.class})
-    public SqlExplainInterceptor sqlExplainInterceptor() {
-        SqlExplainInterceptor sqlExplainInterceptor = new SqlExplainInterceptor();
-        List<ISqlParser> sqlParserList = new ArrayList<>(1);
-        sqlParserList.add(new BlockAttackSqlParser());
-        sqlExplainInterceptor.setSqlParserList(sqlParserList);
-        return sqlExplainInterceptor;
+    @ConditionalOnMissingBean(ConfigurationCustomizer.class)
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> {
+            configuration.setUseDeprecatedExecutor(false);
+            configuration.setDefaultEnumTypeHandler(MybatisEnumTypeHandler.class);
+        };
     }
 
     /**
      * 日期字段自动填充
      */
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(MetaObjectHandler.class)
     @ConditionalOnProperty(value = "ultron.mybatis-plus.auto-fill.enable", matchIfMissing = true)
     public MetaObjectHandler metaObjectHandler(MybatisPlusAutoFillProperties properties) {
         return new UltronMetaObjectHandler(properties);
-    }
-
-    /**
-     * 乐观锁
-     */
-    @Bean
-    @ConditionalOnMissingBean
-    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
-        return new OptimisticLockerInterceptor();
     }
 
 }
