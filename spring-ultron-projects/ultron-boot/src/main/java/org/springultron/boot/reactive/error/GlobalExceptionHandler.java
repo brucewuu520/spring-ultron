aa3fe6fca882e7c6f97de875ec785aadb9959166
brucewuu <402432291@qq.com>
2023-05-19 17:26:47 +0800

    spring-cloud-gateway组件升级

--- spring-ultron-projects/ultron-boot/src/main/java/org/springultron/boot/reactive/error/GlobalExceptionHandler.java
+++ spring-ultron-projects/ultron-boot/src/main/java/org/springultron/boot/reactive/error/GlobalExceptionHandler.java
@@ -2,26 +2,15 @@ package org.springultron.boot.reactive.error;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.util.UriComponentsBuilder;
-import org.springultron.boot.error.UltronErrorEvent;
-import org.springultron.boot.reactive.context.ReactiveRequestContextHolder;
-import org.springultron.core.exception.CryptoException;
-import org.springultron.core.exception.Exceptions;
 import org.springultron.core.exception.ApiException;
+import org.springultron.core.exception.CryptoException;
 import org.springultron.core.result.ApiResult;
 import org.springultron.core.result.ResultCode;
-import org.springultron.core.utils.ObjectUtils;
 import reactor.core.publisher.Mono;
-
-import java.time.LocalDateTime;
 
 /**
  * 通用自定义异常、未知异常处理
@@ -35,13 +24,6 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    private final ApplicationEventPublisher publisher;
-
-    @Autowired
-    public GlobalExceptionHandler(ApplicationEventPublisher publisher) {
-        this.publisher = publisher;
-    }
-
     /**
      * 自定义 REST 业务异常处理
      *
@@ -51,11 +33,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = ApiException.class)
     public Mono<ApiResult<Object>> handleApiException(ApiException e) {
         log.error("自定义业务异常", e);
-
-        // 发送：未知异常异常事件
-        return ReactiveRequestContextHolder.getRequest()
-                .doOnSuccess(r -> publishEvent(r, e))
-                .flatMap(r -> Mono.just(ApiResult.apiException(e)));
+        return Mono.just(ApiResult.apiException(e));
     }
 
     /**
@@ -67,45 +45,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = CryptoException.class)
     public Mono<ApiResult<Object>> handleCryptoException(CryptoException e) {
         log.error("加解密异常", e);
-        // 发送：未知异常异常事件
-        return ReactiveRequestContextHolder.getRequest()
-                .doOnSuccess(r -> publishEvent(r, e))
-                .flatMap(r -> Mono.just(ApiResult.fail(ResultCode.SIGN_FAILED)));
+        return Mono.just(ApiResult.fail(ResultCode.SIGN_FAILED));
     }
 
     @ExceptionHandler(AssertionError.class)
     public Mono<ApiResult<Object>> handleAssertionError(AssertionError e) {
         log.error("断言异常", e);
-        // 发送：未知异常异常事件
-        return ReactiveRequestContextHolder.getRequest()
-                .doOnSuccess(r -> publishEvent(r, e))
-                .flatMap(r -> Mono.just(ApiResult.fail(ResultCode.ASSERTION_ERROR.getCode(), e.getMessage())));
+        return Mono.just(ApiResult.fail(ResultCode.ASSERTION_ERROR.getCode(), e.getMessage()));
     }
 
-    private void publishEvent(ServerHttpRequest request, Throwable error) {
-        UltronErrorEvent event = new UltronErrorEvent();
-        // 请求方法名
-        event.setRequestMethod(request.getMethodValue());
-        // 拼接地址
-        String path = request.getPath().pathWithinApplication().value();
-        MultiValueMap<String, String> queryParams = request.getQueryParams();
-        String requestUrl = UriComponentsBuilder.fromPath(path).queryParams(queryParams).build().toUriString();
-        event.setRequestUrl(requestUrl);
-        // 打印堆栈信息
-        event.setStackTrace(Exceptions.getStackTraceString(error));
-        event.setExceptionName(error.getClass().getName());
-        event.setMessage(error.getMessage());
-        event.setCreatedAt(LocalDateTime.now());
-        StackTraceElement[] elements = error.getStackTrace();
-        if (ObjectUtils.isNotEmpty(elements)) {
-            // 打印控制器类信息
-            StackTraceElement element = elements[0];
-            event.setClassName(element.getClassName());
-            event.setFileName(element.getFileName());
-            event.setMethodName(element.getMethodName());
-            event.setLineNumber(element.getLineNumber());
-        }
-        // 发布事件
-        publisher.publishEvent(event);
-    }
 }
